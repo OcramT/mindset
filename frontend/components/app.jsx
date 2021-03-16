ff6c8654656c34f17ce6d474d827303808ae4e37
Marco Torre <marco.a.torre@gmail.com>
2021-03-16 18:43:50 -0400

    Created and rendered Splash and Splash Container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 // import { Route } from 'react-router-dom';
+import SplashContainer from './splash/splash_container';
 
 const App = () => (
     <div>
-        <h1>MindSet with ReactRouter</h1>
+        <header>
+            <h1>MindSet with ReactRouter</h1>
+            <SplashContainer />
+        </header>
         {/* <Route path="/" /> */}
     </div>
     
