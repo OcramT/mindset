b68e0f2a15ea07f8d1b20f053eafde2b2864b2eb
Marco Torre <marco.a.torre@gmail.com>
2021-03-16 02:16:57 -0400

    Tested and debugged Ajax requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,38 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    
+    private
+
+    def current_user
+        @current_user = User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login(user)
+        @current_user = user
+        session[:session] = @current_user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        session_token[:session_token] = nil
+        @current_user = nil
+    end
+
+    # def require_logged_in
+    #     render 'api/users/show' unless logged_in?
+    # end
+
+    def require_logged_in
+        unless current_user
+            render json: ['Invalid Credentials'], status: 401
+        end
+    end
+
+    # def require_logged_out
+    #     redirect_to root_url if !logged_in?
+    # end
 end
