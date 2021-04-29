d48ed7674d3c5efc5d310f64fd571d01145f431f
OcramT <73545638+OcramT@users.noreply.github.com>
2021-04-29 18:43:32 -0400

    Add audio scrubbing

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import TestMed from '../../../app/assets/audio/TestMeditation.mp3';
-import { fetchMeditation } from '../../util/meditation_api_util';
+import React from 'react';
 
 class Player extends React.Component {
     constructor(props) {
@@ -9,47 +6,72 @@ class Player extends React.Component {
         this.state = {
             playing: false,
             progress: 0,
+            seeking: false,
             currentTime: 0,
-            setCurrentTime: 0,
             duration: 0,
-            setDuration: 0,
-            isSeeking: false,
-            setTime: () => { },
+            completed: false
         }
         this.currentMed = this.props.currentMed
         this.audio = new Audio(this.props.currentMedUrl)
         this.handlePlay = this.handlePlay.bind(this)
-    }
-
-    startTimer() {
-        setTrackProgress(this.audio.currentTime)
-        setInterval(() => {
-            setTrackProgress(this.audio.currentTime.currentTime)
-        }, [1000]);
+        this.onScrub = this.onScrub.bind(this);
+        this.onScrubEnd = this.onScrubEnd.bind(this);
     }
 
     componentDidMount() {
         this.audio.addEventListener("timeupdate", e => {
             this.setState({
-                currentTime: e.target.currentTime,
-                duration: e.target.duration
+                currentTime: this.audio.currentTime,
+                duration: this.audio.duration
             });
         });
     }
 
     componentWillUnmount() {
-        this.audio.removeEventListener("timeupdate", () => { })
+        this.audio.removeEventListener("timeupdate", () => {})
         this.audio.pause()
+    }
+
+    startTimer() {
+        setInterval(() => {
+            if (this.audio.ended) {
+                return this.setState({ completed: true })
+            } else {
+                return this.setState({ progress: this.audio.currentTime })
+            }
+        }, [1000]);
     }
 
     handlePlay() {
         if (!this.state.playing) {
-            this.setState({ playing: true })
-            return this.audio.play()
+            this.setState({ 
+                playing: true 
+            })
+            this.audio.play()
         } else {
             this.setState({ playing: false })
-            return this.audio.pause()
+            this.audio.pause()
         }
+        this.startTimer()
+    }
+
+    onScrub(value) {
+        if (this.state.playing) {
+            this.audio.pause()
+            this.setState({playing: false})
+        } 
+        this.setState({
+            progress: value,
+            currentTime: value,
+        })
+    }
+
+    onScrubEnd() {
+        if (!this.state.playing) {
+            this.audio.currentTime = this.state.currentTime
+        }
+        this.handlePlay()
+        console.log(this.state)
     }
 
     formatTime(seconds) {
@@ -67,20 +89,30 @@ class Player extends React.Component {
 
         return (
             <>
-            {/* <div className='day'>Day 2/30</div> */}
-            <div className='time'>{this.currentMed.duration} minutes</div>
-            <div onClick={() => this.handlePlay()}>
-                {!this.state.playing
-               ? <div className='button-wrapper'>
-                    <img className='med-button' src={medButton} />
-                    <img className='play-button' src={medPlay} />
+                {/* <div className='day'>Day 2/30</div> */}
+                <div className='time'>{this.currentMed.duration} minutes</div>
+                <div onClick={() => this.handlePlay()}>
+                    {!this.state.playing
+                        ? <div className='button-wrapper'>
+                            <img className='med-button' src={medButton} />
+                            <img className='play-button' src={medPlay} />
+                        </div>
+                        : <div className='button-wrapper'>
+                            <img className='med-button' src={medPause} />
+                        </div>}
                 </div>
-                :  <div className='button-wrapper'>
-                    <img className='med-button' src={medPause} />
-                </div>}
-            </div>
-            {/* <div className='progress-bar'></div> */}
-            <div className='timer'>{this.formatTime(this.audio.currentTime)}</div>
+                <input 
+                    type='range' 
+                    value={this.state.progress}
+                    min="0"
+                    max={this.state.duration}
+                    step="1"
+                    onMouseDown={() => this.handlePlay()}
+                    onChange={(e) => this.onScrub(e.target.value)}
+                    onMouseUp={this.onScrubEnd}
+                    onKeyUp={this.onScrubEnd}
+                    className='progress-bar'/>
+                <div className='timer'>{this.formatTime(this.state.currentTime)}</div>
             </>
         )
     }
